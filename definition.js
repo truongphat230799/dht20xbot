0796694dec96953e80a91e59d65019695c7bb2c2
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-07-10 01:25:42 +0700

    Add files via upload

---
+++ definition.js
@@ -0,0 +1,66 @@
+Blockly.Blocks["xbot_dht20_measure"] = {
+  init: function() {
+    this.jsonInit({
+      message0: "cập nhật giá trị cảm biến nhiệt độ độ ẩm",
+      args0: [
+      ],
+      previousStatement: null,
+      nextStatement: null,
+      colour: "#CC6600",
+      tooltip: "",
+      helpUrl: ""
+    });
+  },
+  getDeveloperVars: function() {
+    return ['xbot_dht20_measure'];
+  }
+};
+
+Blockly.Python["xbot_dht20_measure"] = function(block) {
+  // TODO: Assemble Python into code variable.
+    var code = "DHT20.read_dht20()\n";
+  return code;
+};
+
+Blockly.Blocks["xbot_dht20_read"] = {
+  init: function() {
+    this.jsonInit({
+      message0: "đọc giá trị %1 cảm biến DHT20 cổng %2 ",
+      args0: [
+        {
+          type: "field_dropdown",
+          name: "DATA",
+          options: [
+            ["nhiệt độ", "TEMP"],
+            ["độ ẩm", "HUMID"]
+          ]
+        },
+        {
+            type: "field_dropdown",
+            name: "port",
+            options: [
+                ["4","4"]
+            ]
+        },
+      ],
+      output: null,
+      colour: "#CC6600",
+      tooltip: "",
+      helpUrl: ""
+    });
+  }
+};
+
+Blockly.Python["xbot_dht20_read"] = function(block) {
+  var dropdown_data = block.getFieldValue("DATA");
+  // TODO: Assemble Python into code variable.
+  Blockly.Python.definitions_['import_dht20'] = 'from xbot_dht20 import DHT20';
+  //Blockly.Python.definitions_['import_dht20_create'] = 'from lcd_1602 import LCD1602';
+  var code = "";
+  if (dropdown_data == "TEMP")
+    code = "DHT20.dht20_temperature()";
+  else 
+    code = "DHT20.dht20_humidity()";
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
