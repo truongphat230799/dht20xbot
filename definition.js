e951888c7c6b8fe8b0a016bbc4af0a1f9f98f1ea
truongphat230799 <60847675+truongphat230799@users.noreply.github.com>
2022-07-10 23:00:17 +0700

    Update definition.js

--- definition.js
+++ definition.js
@@ -18,7 +18,7 @@ Blockly.Blocks["xbot_dht20_measure"] = {
 
 Blockly.Python["xbot_dht20_measure"] = function(block) {
   // TODO: Assemble Python into code variable.
-    var code = "DHT20.read_dht20()\n";
+    var code = "xbotdht20.read_dht20()\n";
   return code;
 };
 
@@ -55,12 +55,13 @@ Blockly.Python["xbot_dht20_read"] = function(block) {
   var dropdown_data = block.getFieldValue("DATA");
   // TODO: Assemble Python into code variable.
   Blockly.Python.definitions_['import_dht20'] = 'from xbot_dht20 import DHT20';
+  Blockly.Python.definitions_["import_create_dht20"] = "xbotdht20 = DHT20(SoftI2C(scl=Pin(22), sda=Pin(21))";
   //Blockly.Python.definitions_['import_dht20_create'] = 'from lcd_1602 import LCD1602';
   var code = "";
   if (dropdown_data == "TEMP")
-    code = "DHT20.dht20_temperature()";
+    code = "xbotdht20.dht20_temperature()";
   else 
-    code = "DHT20.dht20_humidity()";
+    code = "xbotdht20.dht20_humidity()";
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_NONE];
 };
