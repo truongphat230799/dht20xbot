016c7a0a10e3e4ea09b76da160ebc0b149e42f5b
truongphat230799 <taphat1753@gmail.com>
2023-05-20 07:38:01 +0700

    Update xbot_dht20.py

--- xbot_dht20.py
+++ xbot_dht20.py
@@ -69,10 +69,10 @@ class DHTApi:
         return round(humidity, 1)
 
 class DHT20(DHTApi):
-    def __init__(self,port, i2c):        
-#        self.i2c = i2c
-#       if (self.dht20_read_status() & 0x80) == 0x80:
-#          super().dht20_init()  
+    def __init__(self,port, i2c=0x38):        
+        self.i2c = i2c
+        if (self.dht20_read_status() & 0x80) == 0x80:
+          super().dht20_init()  
         # Grove port: GND VCC SCL SDA
         scl_pin = machine.Pin(PORTS_DIGITAL[port][0])
         sda_pin = machine.Pin(PORTS_DIGITAL[port][1])
